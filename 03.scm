35703c5e1eb80feca284c2e50a1c525fef92dbe2
Mike Belanger <mikejamesbelanger@gmail.com>
2015-02-22 13:53:29 -0500

    Added chapter three, beginning of part 2

---
+++ 03.scm
@@ -0,0 +1,34 @@
+;Answers to Chapter 3 - (Part 2) - Composition of Functions
+;Boring Exercises
+;
+;3.2
+;Alonzo needs to hire 3 people in the first expression, 4 people for the second,
+;and ten people for the third.
+
+;3.3
+;There are four sub-expressions in the first one, three on the second, and three
+;on the last
+
+;3.4
+;Julie is given the job of plus, Jen is given the job of multiplying, Bronwen
+;is given the job of subtracting, as well as Jenna, Andrea and Sarah.  Jenna,
+;Andrea and Sarah.  Bronwen gives her result to Jen, Sarah gives her result to
+;Andrea.  Andrea and Jen both give their results to Julie.
+
+;3.5
+;First one evaluates to 4, second one to 10.
+;
+;3.7
+;(/ 1 3) evaluates to 1/3.
+;
+;3.8
+;Pretty much all the math operators like / * + -
+
+;Real Exercises
+;
+;3.9
+10
+(+ 5 5)
+(+ 2 3 5)
+(+ 5 (+ 2 1)(+ 1 4))
+(display (+ 2 1 (+ 4 3)))
