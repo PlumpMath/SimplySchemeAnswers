bf7f83d3b82f3cc78b88733c90ed197bf8d5f6bd
Mike Belanger <mikejamesbelanger@gmail.com>
2015-03-21 15:22:56 -0400

    Started exercise 4, still have to figure out negatives on scientific function

---
+++ 04.scm
@@ -0,0 +1,44 @@
+;;Real exercises (original exercises are as it says, plain boring :)
+
+;;This requires square from the boring exercises though.
+(define (square x)
+    (* x x))
+;;4.5
+
+(define (fahrenheit_to_celsius temp)
+    (* (- temp 32) (/ 5 9)))
+
+(define (celsius_to_fahrenheit c_temp)
+    (+ (* (/ 9 5) c_temp) 32))
+
+;;4.6
+;;first-way
+(define (fourth x)
+    (* x x x x))
+
+;;second-way
+(define (other_fourth2 x)
+    (square (square x)))
+
+;;4.7
+(define (absolute_value x)
+    (sqrt (square x)))
+
+;;4.8
+;;Note:  I couldn't think of a way of doing this notation without using
+;;some kind of exponent function first.
+
+(define (exp a b)
+    (cond ((= b 0) 1)
+        ((= b 1) a)
+        ((even? b) (exp (square a) (/ b 2)))
+        (else (* a (exp a (+ b -1))))))
+
+;;Now for the actual scientific notation function.  For some reason, this still
+;;doesn't work with negatives.
+;;I should look into this.
+
+(define (scientific a b)
+    (* (exp 10 b) a))
+
+(display (scientific 42 5))
